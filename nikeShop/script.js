2ac3ed3baf9984d533524bb3801e0deca6bce12a
Monke72 <144882433+Monke72@users.noreply.github.com>
2024-03-04 15:43:13 +0400

    Add files via upload

--- nikeShop/script.js
+++ nikeShop/script.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3.1,
+  slidesPerView: 3,
   spaceBetween: 12,
   freeMode: true,
   navigation: {
@@ -8,12 +8,56 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-var swiperToo = new Swiper(".mySwiperToo", {
-  slidesPerView: 3.1,
+var swiperToo = new Swiper(".mySwiperToo-1", {
+  slidesPerView: 2,
   spaceBetween: 12,
   freeMode: true,
   navigation: {
-    nextEl: ".mens__buttons-too",
-    prevEl: ".mens__buttons",
+    nextEl: ".gearup__sect-mens .mens__buttons-too",
+    prevEl: ".gearup__sect-mens .mens__buttons",
   },
 });
+
+var swiperToo = new Swiper(".mySwiperToo-2", {
+  slidesPerView: 2,
+  spaceBetween: 12,
+  freeMode: true,
+  navigation: {
+    nextEl: ".gearup__sect-womens .womens__buttons-too",
+    prevEl: ".gearup__sect-womens .womens__buttons",
+  },
+});
+
+const filters = Array.from(document.querySelectorAll(".filter input"));
+const products = Array.from(
+  document.querySelectorAll(".catalog .catalog__item")
+);
+
+filters.filter((filter) => {
+  filter.addEventListener("click", () => {
+    const checked = checkedInput();
+    const filteredProducts = filteredPoructs(checked);
+
+    console.log(filteredProducts);
+  });
+});
+
+function filteredPoructs(genders) {
+  const idInputs = genders.map((gender) => gender.getAttribute("id"));
+
+  return products.filter((product, index) => {
+    const dataProduct = product.dataset.gender;
+
+    if (dataProduct === idInputs[index]) {
+      return product;
+    }
+  });
+}
+
+function checkedInput() {
+  return filters.filter((filter) => {
+    if (filter.checked) {
+      return filter;
+    }
+  });
+}
